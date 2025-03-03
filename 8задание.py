adfd13061b4be9228d12996915f0090981418014
rinaux <Bazhanovaem@gmail.com>
2025-03-03 20:34:01 +0300

    Update 8задание.py

--- 8задание.py
+++ 8задание.py
@@ -9,4 +9,13 @@ for p in re.finditer(r'(([1-9]+|[abc][1-9a-c]*)\+)*([1-9]+|[abc][1-9a-c]*)', s):
 print(m)
 print(len(m))
 
+import re
 
+s = open('4_0224.txt').readline().strip()
+
+m = ''
+for p in re.finditer(r'((0|[6-9][0,6-9]*)[-*])*(0|[6-9][0,6-9]*)', s):
+    t = p.group(0)
+    m = max(m, t, key=lambda x: len(x))
+print(m)
+print(len(m))
