7b8ac9106f82243d8d5b3ce4dacaa0b841f88e7b
rinaux <Bazhanovaem@gmail.com>
2025-03-03 19:38:06 +0300

    Add files via upload

---
+++ words8.py
@@ -0,0 +1,59 @@
+from itertools import product
+'''i = 0
+k = 0
+a = []
+for p in product(sorted('АМРС'), repeat=5):
+    w = ''.join(p)
+    i += 1
+    if i % 2 == 1 and w[-1] != 'Р' and w.count('А') == 1:
+        k += 1
+        print(i, w)
+        a.append(w)
+print(a.index('ДОВОД')+1)
+print(k)'''
+
+
+'''a = []
+for p in product('КОМПАС', repeat = 6):
+    w = ''.join(p)
+    if len(w) == len(set(w)) and w[0] != 'П' and 'АО' not in w:
+        a.append(w)
+print(a)
+print(len(a))'''
+
+
+'''a = []
+for p in product ('0123', repeat = 5):
+    w = ''.join(p)
+    if w[0] != '0' and '02' not in w and '20' not in w and '13' not in w and '31' not in w:
+        a.append(w)
+print(a)
+print(len(a))'''
+
+'''a = []
+for p in product('АИПРТ', repeat=5):
+    w = ''.join(p)
+    a.append(w)
+print(a.index('АТИПА')+1)'''
+'''i = 0
+k = 0
+a = []
+for p in product('0123456789', repeat = 5):
+    w = ''.join(p)
+    i += 1
+    if len(w) == len(set(w)) and (i % 2 == 0 or i % 2 == 1)and w[-1] == '9' and w[-2] == '2':
+        k += 1
+        print(i, w)
+        a.append(w)
+print(k)'''
+'''print(a.index('ДОВОД')+1)'''
+a = []
+for p in product('КОМПАС', repeat = 6):
+    w = ''.join(p)
+    if w[-1] not in 'ИЕОА' and w[0] not in 'ИЕОА':
+        t = w.replace('И', 'А').replace('Е','А').replace('О','А')
+        t = t.replace('Г', 'Б').replace('П','Б').replace('Р','Б').replace('Л','Б')
+        if 'БАБ' not in t:
+            a.append(w)
+print(a)
+print(len(a))
